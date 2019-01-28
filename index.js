06493ef24050fb876b614d4757377ff0fb39ea41
Joseph Spencer <spencerj21@chcstudent.com>
2019-01-28 15:58:53 -0500

    fixed dependency header

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 
-const nc = require('netcat');
+const NetcatServer = require('netcat/server');
+const NetcatClient = require('netcat/client');
+const ncs = new NetcatServer();
+const ncc = new NetcatClient();
+
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
