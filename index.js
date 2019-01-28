c93f4b4b862a9a1f5ebddff703381ef1129b5c13
walterj21 <walterj21@chcstudent.com>
2019-01-28 18:32:50 -0500

    Added basic stdin/stdout functionality to index for demo purposes

--- index.js
+++ index.js
@@ -7,3 +7,6 @@ const ncc = new NetcatClient();
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
+
+ncs.port(8081).k().listen().pipe(process.stdout); // Pipe all incoming data from port 8081 to standard output; keep the connection alive after a termination
+console.log(process.stdout); // Display standard output
