b27772bc9aa993d49bc0e49f3ad29b5c6766dbc4
walterj21 <walterj21@chcstudent.com>
2019-01-29 09:25:04 -0500

    Event-based listener works better, but packets are output raw in hex.

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-ncs.port(8081).k().listen().pipe(process.stdout); // Pipe all incoming data from port 8081 to standard output; keep the connection alive after a termination
-console.log(process.stdout); // Display standard output
-
-ncs.port(8081).k().listen().on('data', (socket, chunk) => { // Alternative event based method to the above
+ncs.port(8081).k().listen().on('data', (socket, chunk) => { // Event based listener; listens on port 8081 and outputs received packets in hex
     console.log(chunk);
 });
