e08fa867bc491bd2e55fcb6f243dc2cc6203429d
walterj21 <walterj21@chcstudent.com>
2019-02-02 16:40:58 -0500

    Big one. Adapted the original Snow Day Bot to the client, although scraping is not yet set to an interval. Still need to be able to send any messages to the server w/ MSG tag. Added required modules. The client still needs error handling for situations where the server goes out; preferably it will then attempt to connect every few seconds without killing the process.

--- client.js
+++ client.js
@@ -1,13 +1,53 @@
 const net = require('net');
+const rp = require('request-promise');
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+var lastNotification = '';
+target = 2;
 
 // Define switch cases
 const disconnectPrefix = 'END';
+
+// Scraping - Adapted from original Snow Day Bot
+async function scrape() {
+
+    switch(target) {
+        case 0:
+            return rp('https://www.calverthall.com/page').catch((e) => console.error('\x1b[41m%s\x1b[0m',e));
+            break;
+
+        case 1:
+            return fs.readFileSync('./Calvert Hall - Normal.html');
+            break;
+
+        case 2:
+            return fs.readFileSync('./Calvert Hall - Snow Day.html');
+            break;
+
+        default:
+            console.error("\x1b[41m%s\x1b[0m", `invalid target: ${target}`);
+    }
+}
+
+// Parse message - Adapted from original Snow Day Bot
+async function owo() {
+
+    let $ = cheerio.load(await scrape());
+    let notification = $('.message').first().text().trim();
+    if(notification == lastNotification) return;
+    lastNotification = notification;
+    if(!notification) return;
+    console.log(notification); // Log for testing purposes
+
+}
 
 // Establish a connection to the server
 const client = net.createConnection({port: 8081}, {host: 'localhost'}, () => { // Change host variable according to server location
 
     console.log('[CONNECTING]');
     client.write('PNG'); // Fake ping report for testing purposes
+    owo(); // Scrape the CHC site and parse the banner message, if any
 
 });
 
