32a8a975438d42f305415c83acbb37ce25133d7d
Joseph Spencer <spencerj21@chcstudent.com>
2019-02-19 09:52:01 -0500

    impl discord.js and clean up

--- client.js
+++ client.js
@@ -4,7 +4,7 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 
 var lastNotification = '';
-let target = 2;
+var target = 2;
 var pDef, interDef; 
 var interval, timeout = null;
 
@@ -16,46 +16,47 @@ function Missive(tag, content) { // Construct the Missive class
 const disconnectPrefix = 'END'; // Define tags
 const idPrefix = 'ID';
 
-function connect() { // Connect
-    async function scrape() { // Scraping - Adapted from original Snow Day Bot
-        switch (target) {
-            case 0:
-                return rp('https://www.calverthall.com/page').catch((e) => console.error('\x1b[41m%s\x1b[0m', e));
+async function scrape() { // Scraping - Adapted from original Snow Day Bot
+    switch (target) {
+        case 0:
+            return rp('https://www.calverthall.com/page').catch((e) => console.error('\x1b[41m%s\x1b[0m', e));
 
-            case 1:
-                return fs.readFileSync('./Calvert Hall - Normal.html');
+        case 1:
+            return fs.readFileSync('./Calvert Hall - Normal.html');
 
-            case 2:
-                return fs.readFileSync('./Calvert Hall - Snow Day.html');
+        case 2:
+            return fs.readFileSync('./Calvert Hall - Snow Day.html');
 
-            default:
-                console.error('\x1b[41m%s\x1b[0m', `[${Date()}] INVALID TARGET: ${target}`);
-        }
+        default:
+            console.error('\x1b[41m%s\x1b[0m', `[${Date()}] INVALID TARGET: ${target}`);
     }
+}
 
-    async function parse() { // Parse message - Adapted from original Snow Day Bot
-        console.log(`${Date()} Start`); // ###Uncomment this to enable real-time ping-time notifications###
-        let $ = cheerio.load(await scrape());
-        let msg = new Missive('MSG', $('.message').first().text().trim());
-        if (msg.content == lastNotification) return;
-        lastNotification = msg.content;
-        if (!msg.content) return;
-        client.write(JSON.stringify(msg)); // Update latest message on server
-    }
+async function parse() { // Parse message - Adapted from original Snow Day Bot
+    console.log(`${Date()} Start`); // ###Uncomment this to enable real-time ping-time notifications###
+    let $ = cheerio.load(await scrape());
+    let msg = new Missive('MSG', $('.message').first().text().trim());
+    if (msg.content == lastNotification) return;
+    lastNotification = msg.content;
+    if (!msg.content) return;
+    client.write(JSON.stringify(msg)); // Update latest message on server
+}
 
-    async function pSched() { // Schedule theh ping interval
-        timeout = setTimeout(() => {
-            parse();
-            interval = setInterval(parse, interDef);
-        }, pDef);
-    }
+async function pSched() { // Schedule theh ping interval
+    timeout = setTimeout(() => {
+        parse();
+        interval = setInterval(parse, interDef);
+    }, pDef);
+}
 
-    async function antiSched() { // Remove scheduling to avoid stupid dumb loops / memory leaks / duped clients
-        clearInterval(interval);
-        clearTimeout(timeout);
-    }
+async function antiSched() { // Remove scheduling to avoid stupid dumb loops / memory leaks / duped clients
+    clearInterval(interval);
+    clearTimeout(timeout);
+}
 
-    const client = new net.Socket();
+const client = new net.Socket();
+
+function connect() { // Connect  
     client.connect({
         port: 8081
     }, {
@@ -73,24 +74,21 @@ function connect() { // Connect
     client.on(`data`, (res) => { // Handle data
         let obj = JSON.parse(res);
 
+        console.log(`[${Date()}] ${obj.tag}: ${obj.content}`);
+
         switch (obj.tag) {
             case disconnectPrefix: // Abort client if there is an emergency disconnect signal
-                console.log(obj.content);
                 console.log(`[${Date()}] EMERGENCY DISCONNECT`);
                 client.end()
                 break;
 
             case idPrefix: // Inform client of number of active clients for ping scheduling
-                console.log(obj);
                 pDef = obj.content * obj.interval;
                 interDef = obj.active * obj.interval;
 
                 antiSched();
                 pSched();
                 break;
-
-            default:
-                console.log(obj.content);
         }
     });
 
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const net = require('net');
 const fs = require('fs');
+const Discord = require("Discord.js");
+
+const client = new Discord.Client();
+
 
 function Missive(tag, content) {    // Construct the Missive class
     this.tag = tag;
@@ -14,94 +18,107 @@ const registerPrefix = `REG`;
 var socketRegistry = []; // Define misc. variables
 var idFinder = 0;
 
-const server = net.createServer((socket) => {   // Callback here is called on event 'connection,' and returns a socket object to the connection
-    socketRegistry.push(socket);    // Add new socket to the registry
+const token = process.env.SNOWDAYBOTTOKEN;
+const announcementChannels = ['475116538773372988'];//,'534074116399955968','388480229938823168'];
+const useEveryone = false;
 
-    socket.setEncoding('utf8');
-    socket.on('data', (req) => {    // Assign event callbacks
-        try {   // Error handling for JSON strings
-            JSON.parse(req);
-        } catch(err) {
-            console.error("Bad String");
-            return;
-        }
+client.login(token);
 
-        let obj = JSON.parse(req); // Convert JSON string to an object
+console.log("\x1b[44m%s\x1b[0m","Initializing...");
+client.on("ready", () => {
+    console.log("\x1b[44m%s\x1b[0m","Initialized");
 
-        switch (obj.tag) {
-            case messagePrefix: // Log snow message and post it to Discord
-                if (obj.content == fs.readFileSync('lastNotification.log')) break;
-                console.log(`[${Date()}] Snow day! ${obj.content}`);
+    const server = net.createServer((socket) => {   // Callback here is called on event 'connection,' and returns a socket object to the connection
+        socketRegistry.push(socket);    // Add new socket to the registry
 
-                // Add stuff
+        socket.setEncoding('utf8');
+        socket.on('data', (req) => {    // Assign event callbacks
+            try {   // Error handling for JSON strings
+                JSON.parse(req);
+            } catch(err) {
+                console.error("Bad String");
+                return;
+            }
 
-                fs.writeFileSync('lastNotification.log', obj.content);
+            let obj = JSON.parse(req); // Convert JSON string to an object
 
-                break;
+            switch (obj.tag) {
+                case messagePrefix: // Log snow message and post it to Discord
+                    if (obj.content == fs.readFileSync('lastNotification.log')) break;
+                    console.log(`[${Date()}] Snow day! ${obj.content}`);
 
-            case disconnectPrefix: // Stop communication with the bots
-                let end = new Missive('END', `[${Date()}] END SIGNAL`)
-                console.log(`[${Date()}] Emergency communications shutoff - Disconnecting from bots...`);
+                    for(let channelId of announcementChannels){
+                        client.channels.get(channelId).send((useEveryone ? "@everyone " : "") + obj.content).catch(console.error);
+                    }
 
-                socketRegistry.forEach((connectionSocket) => {if (!connectionSocket.destroyed) connectionSocket.write(JSON.stringify(end))});
-                break;
+                    fs.writeFileSync('lastNotification.log', obj.content);
 
-            case shutdownPrefix: // Shut down the server
-                console.log(`[${Date()}]  Code ${shutdownPrefix}! Shutting down!`);
-                process.exit();
-                break;
+                    break;
 
-            case registerPrefix: // Add new clients to the network, inform all clients of new ping schedule
-                server.getConnections((err, n) => {
-                    idFinder = 0;
+                case disconnectPrefix: // Stop communication with the bots
+                    let end = new Missive('END', `[${Date()}] END SIGNAL`)
+                    console.log(`[${Date()}] Emergency communications shutoff - Disconnecting from bots...`);
 
-                    if (err) console.error(err);
-                    console.log(`[${Date()}] Client at ${socket.remoteAddress} connected (${n} active)`);
+                    socketRegistry.forEach((connectionSocket) => {if (!connectionSocket.destroyed) connectionSocket.write(JSON.stringify(end))});
+                    break;
 
-                    socketRegistry.forEach((connectionSocket) => {
-                        if (!connectionSocket.destroyed) { 
-                            let id = new Missive('ID', idFinder);
-                            id['active'] = n;
-                            id['interval'] = 2000;
-                            connectionSocket.write(JSON.stringify(id));
-                            idFinder++;
-                        }
+                case shutdownPrefix: // Shut down the server
+                    console.log(`[${Date()}]  Code ${shutdownPrefix}! Shutting down!`);
+                    process.exit();
+                    break;
+
+                case registerPrefix: // Add new clients to the network, inform all clients of new ping schedule
+                    server.getConnections((err, n) => {
+                        idFinder = 0;
+
+                        if (err) console.error(err);
+                        console.log(`[${Date()}] Client at ${socket.remoteAddress} connected (${n} active)`);
+
+                        socketRegistry.forEach((connectionSocket) => {
+                            if (!connectionSocket.destroyed) { 
+                                let id = new Missive('ID', idFinder);
+                                id['active'] = n;
+                                id['interval'] = 2000;
+                                connectionSocket.write(JSON.stringify(id));
+                                idFinder++;
+                            }
+                        });
                     });
+                    break;
+
+                default:
+                    console.log(`[${Date()}] Queer message - Logging...`);  // Log queer messages
+                    fs.appendFile('LOG', `[${Date()}] - ${req.tag + req.content}`, (e) => console.error('\x1b[41m%s\x1b[0m', e));
+            }
+        });
+
+        socket.on('error', (e) => console.error('\x1b[41m%s\x1b[0m', e));
+
+        socket.on('end', () => {
+            server.getConnections((err, n) => { // Log disconnections and active clients
+                idFinder = 0;
+
+                if (err) console.error(err);
+                console.log(`[${Date()}] Client at ${socket.remoteAddress} disconnected (${n} active)`);
+                let index = socketRegistry.indexOf(socket);
+                if(index !== -1) socketRegistry.splice(index, 1);   // Remove socket from registry
+
+                socketRegistry.forEach((connectionSocket) => {  // Update client ID information
+                    if (!connectionSocket.destroyed) {
+                        let id = new Missive('ID', idFinder);
+                        id['active'] = n;
+                        id['interval'] = 2000;
+                        connectionSocket.write(JSON.stringify(id));
+                        idFinder++;
+                    }
                 });
-                break;
-
-            default:
-                console.log(`[${Date()}] Queer message - Logging...`);  // Log queer messages
-                fs.appendFile('LOG', `[${Date()}] - ${req.tag + req.content}`, (e) => console.error('\x1b[41m%s\x1b[0m', e));
-        }
-    });
-
-    socket.on('error', (e) => console.error('\x1b[41m%s\x1b[0m', e));
-
-    socket.on('end', () => {
-        server.getConnections((err, n) => { // Log disconnections and active clients
-            idFinder = 0;
-
-            if (err) console.error(err);
-            console.log(`[${Date()}] Client at ${socket.remoteAddress} disconnected (${n} active)`);
-            let index = socketRegistry.indexOf(socket);
-            if(index !== -1) socketRegistry.splice(index, 1);   // Remove socket from registry
-
-            socketRegistry.forEach((connectionSocket) => {  // Update client ID information
-                if (!connectionSocket.destroyed) {
-                    let id = new Missive('ID', idFinder);
-                    id['active'] = n;
-                    id['interval'] = 2000;
-                    connectionSocket.write(JSON.stringify(id));
-                    idFinder++;
-                }
             });
         });
     });
-});
 
-server.on('error', (e) => console.error('\x1b[41m%s\x1b[0m', e));
+    server.on('error', (e) => console.error('\x1b[41m%s\x1b[0m', e));
 
-server.listen(8081, () => { // Server listens on port 8081
-    console.log(`[${Date()}] Server bound`);
+    server.listen(8081, () => { // Server listens on port 8081
+        console.log(`[${Date()}] Server bound`);
+    });
 });
