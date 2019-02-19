f1559200a7920241d6e08e5ac3d1996a8d29cf99
Jason Walter <jaason@protonmail.ch>
2019-02-19 10:10:55 -0500

    Fixed Joe's mistake again. Joe breaks the client every time he touches it. Joe is not allowed to commit to client.js ever again.

--- client.js
+++ client.js
@@ -16,47 +16,50 @@ function Missive(tag, content) { // Construct the Missive class
 const disconnectPrefix = 'END'; // Define tags
 const idPrefix = 'ID';
 
-async function scrape() { // Scraping - Adapted from original Snow Day Bot
-    switch (target) {
-        case 0:
-            return rp('https://www.calverthall.com/page').catch((e) => console.error('\x1b[41m%s\x1b[0m', e));
-
-        case 1:
-            return fs.readFileSync('./Calvert Hall - Normal.html');
-
-        case 2:
-            return fs.readFileSync('./Calvert Hall - Snow Day.html');
-
-        default:
-            console.error('\x1b[41m%s\x1b[0m', `[${Date()}] INVALID TARGET: ${target}`);
-    }
-}
-
-async function parse() { // Parse message - Adapted from original Snow Day Bot
-    console.log(`${Date()} Start`); // ###Uncomment this to enable real-time ping-time notifications###
-    let $ = cheerio.load(await scrape());
-    let msg = new Missive('MSG', $('.message').first().text().trim());
-    if (msg.content == lastNotification) return;
-    lastNotification = msg.content;
-    if (!msg.content) return;
-    client.write(JSON.stringify(msg)); // Update latest message on server
-}
-
-async function pSched() { // Schedule theh ping interval
-    timeout = setTimeout(() => {
-        parse();
-        interval = setInterval(parse, interDef);
-    }, pDef);
-}
-
-async function antiSched() { // Remove scheduling to avoid stupid dumb loops / memory leaks / duped clients
-    clearInterval(interval);
-    clearTimeout(timeout);
-}
-
-const client = new net.Socket();
 
 function connect() { // Connect  
+
+
+    async function scrape() { // Scraping - Adapted from original Snow Day Bot
+        switch (target) {
+            case 0:
+                return rp('https://www.calverthall.com/page').catch((e) => console.error('\x1b[41m%s\x1b[0m', e));
+
+            case 1:
+                return fs.readFileSync('./Calvert Hall - Normal.html');
+
+            case 2:
+                return fs.readFileSync('./Calvert Hall - Snow Day.html');
+
+            default:
+                console.error('\x1b[41m%s\x1b[0m', `[${Date()}] INVALID TARGET: ${target}`);
+        }
+    }
+
+    async function parse() { // Parse message - Adapted from original Snow Day Bot
+        console.log(`${Date()} Start`); // ###Uncomment this to enable real-time ping-time notifications###
+        let $ = cheerio.load(await scrape());
+        let msg = new Missive('MSG', $('.message').first().text().trim());
+        if (msg.content == lastNotification) return;
+        lastNotification = msg.content;
+        if (!msg.content) return;
+        client.write(JSON.stringify(msg)); // Update latest message on server
+    }
+
+    async function pSched() { // Schedule theh ping interval
+        timeout = setTimeout(() => {
+            parse();
+            interval = setInterval(parse, interDef);
+        }, pDef);
+    }
+
+    async function antiSched() { // Remove scheduling to avoid stupid dumb loops / memory leaks / duped clients
+        clearInterval(interval);
+        clearTimeout(timeout);
+    }
+
+    const client = new net.Socket();
+
     client.connect({
         port: 8081
     }, {
